ff46ae18c8f0b9063ee19383b9dd17fe39e2d10b
Vibhor Joshi <vibhorjoshi05@gmail.com>
2017-12-10 19:58:49 +0530

    Basic Route Setup

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const app = express();
 const port=8000;
+
+require('./app/routes')(app,{});
 app.listen(port, () =>{
   console.log("We are live on "+port);
 })
