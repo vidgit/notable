fc8665ba01a99c20b90928f4413e4c495403ae50
Vibhor Joshi <vibhorjoshi05@gmail.com>
2017-12-12 00:28:30 +0530

    Added Body parser

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const app = express();
 const port=8000;
+app.use(bodyParser.urlencoded({extended:true}));
 
 require('./app/routes')(app,{});
 app.listen(port, () =>{
