4bd38c8400df84fdef71faa2e957e1d70f6f6ffd
Vibhor Joshi <vibhorjoshi05@gmail.com>
2017-12-12 22:20:51 +0530

    Infrastructure Setup Complete

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const app = express();
 const port=8000;
+const db=require('./config/db');
+
 app.use(bodyParser.urlencoded({extended:true}));
 
-require('./app/routes')(app,{});
-app.listen(port, () =>{
-  console.log("We are live on "+port);
-})
+
+
+MongoClient.connect(db.url, (err,database)=>{
+  if(err) return console.log(err);
+  require('./app/routes')(app,database);
+  app.listen(port, () =>{
+    console.log("We are live on "+port);
+  })
+});
